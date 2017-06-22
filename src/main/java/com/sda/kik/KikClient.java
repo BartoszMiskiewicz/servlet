d121e595a3acc3f3068a30b63cd2783e5f238d5a
BartoszMiskiewicz <bartosz-miskiewicz@wp.pl>
2017-06-22 19:37:39 +0200

    added warmup Game

--- src/main/java/com/sda/kik/KikClient.java
+++ src/main/java/com/sda/kik/KikClient.java
@@ -3,5 +3,5 @@ package com.sda.kik;
 /**
  * Created by bartosz on 21.06.2017.
  */
-public class Server {
+public class KikClient {
 }
---
+++ src/main/java/com/sda/warmup/moreless/GameServer.java
@@ -0,0 +1,7 @@
+package com.sda.warmup.moreless;
+
+/**
+ * Created by bartosz on 22.06.2017.
+ */
+public class GameServer {
+}
